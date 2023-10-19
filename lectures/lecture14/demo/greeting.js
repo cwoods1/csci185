68a75dbbda1564aa9e486cc16ae831894b8d2cf9
Cody <cwoods1@unca.edu>
2023-10-18 20:58:53 -0400

    hw5

---
+++ lectures/lecture14/demo/greeting.js
@@ -0,0 +1,11 @@
+function sayHello() {
+    // your code here...
+    document.querySelector("#message").innerHTML = "<strong>Hola muchacho.</strong>";
+    document.querySelector("#message").style.color = "red";
+}
+
+function sayGoodbye() {
+    // your code here...
+    document.querySelector("#message").innerHTML = 'Adios muchacho.';
+    document.querySelector("#message").styl = "img src='https://media1.giphy.com/media/FSR4F54XaEDvLp5iAE/giphy.gif?cid=6c09b9523j0p5323x9t9l1rt8ooystp4f50uprzaf6ta9v82&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g' />";
+}
