45d0b40f89f4be3e571f87625f9f8a9f99494ca2
Cody <cwoods1@unca.edu>
2023-11-01 15:37:48 -0400

    monday class

--- lectures/lecture16/03-drawings/sketch.js
+++ lectures/lecture16/03-drawings/sketch.js
@@ -4,11 +4,11 @@ function setup() {
     createCanvas(canvasWidth, canvasHeight);
     background("black");
 
-    drawCreature(100, 100, 'red', 200);
-    drawCreature(250, 250, 'orange', 250);
-    drawCreature(400, 100, 'yellow', 75);
-    drawCreature(550, 250, 'green', 50);
-    drawCreature(900, 500, 'blue', 300);
+    drawCreature(100, 100, 'red', 200, true);
+    drawCreature(250, 250, 'orange', 250, false);
+    drawCreature(400, 100, 'yellow', 75, true);
+    drawCreature(550, 250, 'green', 50, true);
+    drawCreature(900, 500, 'blue', 300, false);
 
     //triangle(200, 200, 100, 300, 200, 400)
     //triangle(300, 300, 200, 400, 400, 400)
@@ -20,16 +20,27 @@ function setup() {
 }
 
 
-function drawCreature(x, y, color, size) {
+function drawCreature(x, y, color, size, isZombie) {
     fill(color);
     //let size = 100;
     let eyeball = size / 7.5;
     circle(x, y, size); //base shape face
-    fill('white');
+    
+    if (isZombie) {
+        fill ("red");
+    } else {
+            fill ("white")
+        }
+    
+
+
+   // fill('white');
+    
+    
     circle(x + eyeball, y - eyeball, 2 * eyeball); //left eye
     circle(x - eyeball, y - eyeball, 2 * eyeball); //right eye
     fill ('black')
     circle(x + eyeball, y - eyeball, eyeball); //left eye
     circle(x - eyeball, y - eyeball, eyeball); //right eye
 
-}
+    }
