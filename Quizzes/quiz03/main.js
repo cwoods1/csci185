f233a6801d6ea8a9cec92d12cdfe9625f2694073
Cody <cwoods1@unca.edu>
2023-12-04 16:09:17 -0500

    quiz 3

---
+++ Quizzes/quiz03/main.js
@@ -0,0 +1,26 @@
+
+
+function color1() {
+    document.querySelector("#square1").style.backgroundColor = "red";
+}
+
+
+function color2() {
+    document.querySelector("#square2").style.backgroundColor = "orange";
+}
+
+function color3() {
+    document.querySelector("#square3").style.backgroundColor = "yellow";
+}
+
+function color4() {
+    document.querySelector("#square4").style.backgroundColor = "green";
+}
+
+function color5() {
+    document.querySelector("#square5").style.backgroundColor = "blue";
+}
+
+function color6() {
+    document.querySelector("#square6").style.backgroundColor = "purple";
+}
